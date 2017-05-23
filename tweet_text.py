c629ce57ebb8407ebb967f871e1235ca68bafd8d
Arian Treffer <derari@web.de>
2017-05-23 14:35:27 +0200

    Update tweet_text.py

--- tweet_text.py
+++ tweet_text.py
@@ -9,6 +9,24 @@ def reply(tweet):
     """Return text to be used as a reply"""
     message = tweet['text']
     user = tweet['user']['screen_name']
+    
+    if "1+1" in message:
+        return "@" + user + " 2"
+    if "1+2" in message:
+        return "@" + user + " 3"
+    if "2+3" in message:
+        return "@" + user + " 456"
+    if "1999+1" in message:
+        return "@" + user + " 2000"
+    if "9000+1" in message:
+        return "@" + user + " 9001 It's over nine thousand!"
+    if "rly" in message:
+        return "@" + user + " YA RLY!"
+    pokemons = ["Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise"]
+    if "Pokemon" in message:
+        number = message.split('#')[1]
+        return "@" + user + " " + pokemons[int(number)-1]
+    
     if "hi" in message.lower():
         berlin_time = datetime.now(timezone('Europe/Berlin'))
         date = berlin_time.strftime("It is %H:%M:%S on a %A (%d-%m-%Y).")
